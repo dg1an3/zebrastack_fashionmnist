369e70dc2694c8a769cfe9b687deda9ab9df5c60
Lane, Derek <dg1an3@2nd-messenger.com>
2021-05-20 14:51:53 -0500

    adding tf.net solution / project

---
+++ FashionMnistVaeTfNet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static Tensorflow.Binding;
+
+namespace FashionMnistVaeTfNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = tf.constant("Hello, TensorFlow!");
+            Console.WriteLine(hello);
+        }
+    }
+}
